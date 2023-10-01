6c7979513789a1f488fafcda209ff1dac18e5b3d
zmatias7 <146601466+zmatias7@users.noreply.github.com>
2023-09-30 23:01:38 -0300

    Add files via upload

---
+++ personalizado.js
@@ -0,0 +1,7 @@
+function mouseOver(){
+    document.getElementById("mouseAlt").innerHTML = "Retire o mouse";
+}
+
+function mouseOut(){
+    document.getElementById("mouseAlt").innerHTML = "passe o mouse";
+}
